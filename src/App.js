6e6df9a98ef7bf4e7f8c76cf1ebb6c05c2654c4b
Vaishnavi <vaishdw2207@gmail.com>
2021-04-08 17:45:26 +0530

    style: add basic css

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ import "./App.css"
 import Quiz from "./pages/Quiz"
 import Landing from "./pages/Landing"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import React from "react"
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/quiz/:id">
-          <Quiz />
-        </Route>
-        <Route path="/">
-          <Landing />
-        </Route>
-      </Switch>
-    </Router>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      counter: 0,
+      questionId: 1,
+      question: "",
+      answerOptions: [],
+      answer: "",
+      answersCount: {},
+      result: "",
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/quiz/:id">
+            <Quiz />
+          </Route>
+          <Route path="/">
+            <Landing />
+          </Route>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App
