032c1f3b553fe02abad0157fd165b26d41622428
Vaishnavi <vaishdw2207@gmail.com>
2021-04-08 15:08:14 +0530

    chore: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css"
 function App() {
     return (
         <div className="App">
-            <h1> Hi</h1>
+            <h1 className="text-2xl font-bold"> Hi</h1>
         </div>
     )
 }
